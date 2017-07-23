38020ed64371fe99f598f769b13075bc9bec0905
JhonXY <1076849402@qq.com>
2017-07-23 20:10:20 +0800

    address sec

--- src/views/Address.vue
+++ src/views/Address.vue
@@ -60,7 +60,7 @@
         <div class="addr-list-wrap">
           <div class="addr-list">
             <ul>
-              <li v-for="item in addressList">
+              <li v-for="(item,index) in addressListFilter" :class="{'check': checkIndex == index}" @click="checkIndex = index">
                 <dl>
                   <dt>{{item.userName}}</dt>
                   <dd class="address">{{item.streetName}}</dd>
@@ -88,7 +88,7 @@
           </div>
 
           <div class="shipping-addr-more">
-            <a class="addr-more-btn up-down-btn" href="javascript:;">
+            <a class="addr-more-btn up-down-btn" href="javascript:;" @click="expand" :class="{'open': limit > 3}">
               more
               <i class="i-up-down">
                 <i class="i-up-down-l"></i>
@@ -146,7 +146,14 @@ export default {
   },
   data () {
     return {
-      addressList: []
+      addressList: [],
+      limit: 3,
+      checkIndex: 0
+    }
+  },
+  computed: {
+    addressListFilter () {
+      return this.addressList.slice(0,this.limit)
     }
   },
   methods: {
@@ -155,6 +162,13 @@ export default {
         let res = response.data
         this.addressList = res.result
       })
+    },
+    expand () {
+      if (this.limit == 3) {
+        this.limit = this.addressList.length
+      } else {
+        this.limit = 3
+      }
     }
   },
   mounted () {
