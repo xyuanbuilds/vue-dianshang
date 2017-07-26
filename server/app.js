1beb9037f2c8caa74251467c2c371fb6a30368ff
JhonXY <1076849402@qq.com>
2017-07-26 20:34:50 +0800

    modify

--- server/app.js
+++ server/app.js
@@ -12,6 +12,7 @@ var goods = require('./routes/goods');
 
 var app = express();
 
+// 修改读取html
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.html', ejs.__express)
